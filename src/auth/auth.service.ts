d348ec9efb9b76b5dc8c3bb75bb1987706bccd77
alirio <mie21001@byui.edu>
2022-12-10 18:36:45 -0400

    generar jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { LoginInput, SignupInput } from './dto/inputs';
@@ -9,13 +11,20 @@ import { AuthResponse } from './dto/types/auth-response.types';
 export class AuthService {
 
     constructor(
-        private readonly userService: UsersService
+        private readonly userService: UsersService,
+        private readonly jwtService: JwtService
     ) { }
+
+    private getJwtToken( userId: string) {
+        return this.jwtService.sign({ id: userId});
+    }
 
     async signup(signupInput: SignupInput): Promise<AuthResponse> {
         const user = await this.userService.create(signupInput);
-        const token = 'asdasd';
-        return { token: token, user: user }
+
+        const token = this.getJwtToken(user.id);
+        
+        return { token, user: user }
     }
 
     async login(loginInput: LoginInput): Promise<AuthResponse> {
@@ -26,8 +35,10 @@ export class AuthService {
             throw new BadRequestException('Invalid credentials');
         }
 
+        const token = this.getJwtToken(user.id);
+
         return {
-            token: 'asdasd',
+            token,
             user
         }
     }
