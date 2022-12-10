f5a10485ba850fec33ac2e0df72ea19ee30ba803
alirio <mie21001@byui.edu>
2022-12-10 18:19:28 -0400

    jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,30 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { UsersModule } from 'src/users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  providers: [AuthResolver, AuthService],
-  imports: [UsersModule],
-  exports: [AuthService],
+  providers: [AuthResolver, AuthService, JwtStrategy],
+  imports: [
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    
+    JwtModule.registerAsync({
+      imports: [ ConfigModule ],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        console.log(configService.get('JWT_SECRET'))
+        return {
+          secret: configService.get('JWT_SECRET'),
+          expiresIn: '4h'
+        }
+      }
+    }),
+    UsersModule
+  ],
+  exports: [AuthService, JwtStrategy, PassportModule, JwtModule],
 })
-export class AuthModule {}
+export class AuthModule { }
