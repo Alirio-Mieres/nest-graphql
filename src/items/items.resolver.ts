67fd08ccec15f812db74e204e4f925a13b647cfa
alirio <mie21001@byui.edu>
2022-12-26 18:30:25 -0400

    actualizar item

--- src/items/items.resolver.ts
+++ src/items/items.resolver.ts
@@ -1,35 +1,53 @@
 import { Resolver, Query, Mutation, Args, Int, ID } from '@nestjs/graphql';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Item } from './entities/item.entity';
+import { User } from 'src/users/entities/user.entity';
 import { CreateItemInput, UpdateItemInput } from './dto/inputs';
-import { ParseUUIDPipe } from '@nestjs/common';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 
 @Resolver(() => Item)
+@UseGuards(JwtAuthGuard)
 export class ItemsResolver {
   constructor(private readonly itemsService: ItemsService) {}
 
   @Mutation(() => Item)
-  async createItem(@Args('createItemInput') createItemInput: CreateItemInput): Promise<Item> {
-    return this.itemsService.create(createItemInput);
+  async createItem(
+    @Args('createItemInput') createItemInput: CreateItemInput,
+    @CurrentUser() user: User
+    ): Promise<Item> {
+    return this.itemsService.create(createItemInput, user);
   }
 
   @Query(() => [Item], { name: 'items' })
-  async findAll(): Promise<Item[]> {
-    return this.itemsService.findAll();
+  async findAll(
+  @CurrentUser() user: User
+  ): Promise<Item[]> {
+    return this.itemsService.findAll(user);
   }
 
   @Query(() => Item, { name: 'item' })
-  findOne(@Args('id', { type: () => ID }, ParseUUIDPipe) id: string) {
-    return this.itemsService.findOne(id);
+  async findOne(
+    @Args('id', { type: () => ID }, ParseUUIDPipe,) id: string,
+    @CurrentUser() user: User
+    ): Promise<Item> {
+    return this.itemsService.findOne(id, user);
   }
 
   @Mutation(() => Item)
-  updateItem(@Args('updateItemInput') updateItemInput: UpdateItemInput): Promise<Item> {
-    return this.itemsService.update(updateItemInput.id, updateItemInput);
+  updateItem(@
+    Args('updateItemInput') updateItemInput: UpdateItemInput,
+    @CurrentUser() user: User
+    ): Promise<Item> {
+    return this.itemsService.update(updateItemInput.id, updateItemInput, user);
   }
 
   @Mutation(() => Item)
-  removeItem(@Args('id', { type: () => ID }) id: string): Promise<Item> {
-    return this.itemsService.remove(id);
+  removeItem(
+    @Args('id', { type: () => ID }) id: string,
+    @CurrentUser() user: User
+    ): Promise<Item> {
+    return this.itemsService.remove(id, user);
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,45 +1,55 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateItemInput, UpdateItemInput } from './dto/inputs';
 import { Item } from './entities/item.entity';
 
 @Injectable()
 export class ItemsService {
-
   constructor(
     @InjectRepository(Item)
-    private readonly itemRepository: Repository<Item>
-  ) { }
+    private readonly itemRepository: Repository<Item>,
+  ) {}
 
-  async create(createItemInput: CreateItemInput): Promise<Item> {
-    const newItem = this.itemRepository.create(createItemInput);
+  async create(createItemInput: CreateItemInput, user: User): Promise<Item> {
+    const newItem = this.itemRepository.create({ ...createItemInput, user });
     return await this.itemRepository.save(newItem);
   }
 
-  async findAll(): Promise<Item[]> {
-    return await this.itemRepository.find();
+  async findAll(user: User): Promise<Item[]> {
+    return await this.itemRepository.find({
+      where: {
+        user: {
+          id: user.id,
+        },
+      },
+    });
   }
 
-  async findOne(id: string): Promise<Item> {
-    const item = this.itemRepository.findOneBy({ id });
+  async findOne(id: string, user: User): Promise<Item> {
+    const item = await this.itemRepository.findOneBy({ id, user: { id: user.id } });
 
     if (!item) throw new NotFoundException(`Item with id #${id} not found`);
+
+    // item.user = user;
 
     return item;
   }
 
-  async update(id: string, updateItemInput: UpdateItemInput): Promise<Item> {
-    const item = await this.itemRepository.preload(updateItemInput);
+  async update(id: string, updateItemInput: UpdateItemInput, user: User): Promise<Item> {
     
+    await this.findOne(id, user);
+    const item = await this.itemRepository.preload(updateItemInput);
+
     if (!item) throw new NotFoundException(`Item with id #${id} not found`);
 
     return this.itemRepository.save(item);
   }
 
-  async remove(id: string): Promise<Item> {
-    const item = await this.findOne(id);
+  async remove(id: string, user: User): Promise<Item> {
+    const item = await this.findOne(id, user);
     await this.itemRepository.remove(item);
-    return {...item, id};
+    return { ...item, id };
   }
 }
