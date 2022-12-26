80ae5eb41caf5262617e21539ef73eb3d9e409df
alirio <mie21001@byui.edu>
2022-12-26 16:26:19 -0400

    GraphQLModuleforRootAsync

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,44 +4,62 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { JwtService } from '@nestjs/jwt';
 
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { ItemsModule } from './items/items.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+
+    GraphQLModule.forRootAsync({
       driver: ApolloDriver,
-     // debug: false,
-      playground: false,
-      autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
-      plugins: [
-        ApolloServerPluginLandingPageLocalDefault
-        ]
+      imports: [AuthModule],
+      inject: [JwtService],
+      useFactory: async (jwtService: JwtService) => ({
+        playground: false,
+        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        plugins: [ApolloServerPluginLandingPageLocalDefault],
+        context({ req }) {
+          const token = req.headers.authorization?.replace('Bearer ', '');
+          if (!token) throw Error('Token needed');
+
+          const payload = jwtService.decode(token);
+          if (!payload) throw Error('Invalid token');
+        },
       }),
-      
-      TypeOrmModule.forRoot({
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: +process.env.DB_PORT,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        entities: [],
-        synchronize: true,
-        autoLoadEntities: true
-      }),
-      
-      ItemsModule,
-      
-      UsersModule,
-      
-      AuthModule,
+    }),
+
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //  // debug: false,
+    //   playground: false,
+    //   autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
+    //   plugins: [
+    //     ApolloServerPluginLandingPageLocalDefault
+    //     ]
+    //   }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+
+    ItemsModule,
+
+    UsersModule,
+
+    AuthModule,
   ],
   controllers: [],
   providers: [],
